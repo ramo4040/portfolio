e3f79bfae711e8a5ebe3e494f4ab94fe3772b7dc
Yassir Rouane <yasserro100@gmail.com>
2025-06-20 14:36:45 +0100

    feat: YASSIR text component

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,9 +1,11 @@
 import { TopNavbar } from "@/widgets/top-navbar/top-navbar";
+import { Yassir } from "@/widgets/yassir/yassir";
 
 export default function Home() {
 	return (
 		<main>
 			<TopNavbar />
+			<Yassir />
 		</main>
 	);
 }
---
+++ apps/web/src/widgets/yassir/yassir.tsx
@@ -0,0 +1,9 @@
+import "./style.css";
+
+export const Yassir = () => {
+	return (
+		<div id="name-container">
+			<h1>Yassir</h1>
+		</div>
+	);
+};
