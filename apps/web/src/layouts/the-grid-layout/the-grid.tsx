73332ba91b76763fc91c06377aef384c782eb508
Yassir Rouane <yasserro100@gmail.com>
2025-06-20 23:28:35 +0100

    feat: create project card component

--- apps/web/src/layouts/the-grid-layout/the-grid.tsx
+++ apps/web/src/layouts/the-grid-layout/the-grid.tsx
@@ -1,5 +1,7 @@
 import { DefaultCard } from "@/widgets/cards";
 import "./style.css";
+import { ProjectCard } from "@/widgets/cards/project-card/project-card";
+import Image from "next/image";
 
 export const TheGridLayout = () => {
 	return (
@@ -15,6 +17,21 @@ export const TheGridLayout = () => {
 					intuitive interfaces or architecting robust APIs. Always learning,
 					always building—for today’s challenges and tomorrow’s innovations.
 				</p>
+			</DefaultCard>
+
+			<ProjectCard
+				className="col-span-2 row-span-2"
+				theme="#fff"
+				color="#000"
+				imageAlt="Mindsprint Project"
+				imageSrc="/images/projects/mindsprint/mindsprint-bg.png"
+				href="#"
+				title="Mindsprint"
+				description="MindSprint is an AI-powered online learning platform designed to empower developers with personalized, interactive learning experiences."
+			/>
+
+			<DefaultCard className="col-span-1 row-span-1">
+				<Image src="/images/my-location.png" fill alt="My location" />
 			</DefaultCard>
 		</section>
 	);
---
+++ apps/web/src/widgets/cards/project-card/project-card.tsx
@@ -0,0 +1,56 @@
+import { ArrowUpRight } from "lucide-react";
+import "./style.css";
+import Image from "next/image";
+import Link from "next/link";
+import type { ComponentProps, FC } from "react";
+
+type ProjectCardProps = ComponentProps<"div"> & {
+	theme?: string;
+	color?: string;
+	title?: string;
+	description?: string;
+	imageSrc: string;
+	imageAlt: string;
+	href: string;
+};
+
+export const ProjectCard: FC<ProjectCardProps> = ({
+	theme,
+	className,
+	style,
+	color,
+	imageSrc,
+	imageAlt,
+	href,
+	title,
+	description,
+	...props
+}) => {
+	return (
+		<Link href={href} id="project-card-link" className={`${className} card`}>
+			<div
+				id="project-card"
+				style={{ backgroundColor: theme, color: color, ...style }}
+				{...props}
+			>
+				<article>
+					<div className="link">
+						<div className="description">Open Project</div>
+						<div className="icon">
+							<ArrowUpRight size="30px" />
+						</div>
+					</div>
+
+					<div className="image">
+						<Image src={imageSrc} alt={imageAlt} fill />
+					</div>
+
+					<div className="description">
+						<h1>{title}</h1>
+						<p>{description}</p>
+					</div>
+				</article>
+			</div>
+		</Link>
+	);
+};
