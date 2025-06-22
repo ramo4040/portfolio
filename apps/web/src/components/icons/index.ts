695eac4514b03b795d43ffe8aea1387f0955b859
Yassir Rouane <yasserro100@gmail.com>
2025-06-22 18:21:14 +0100

    feat: add social account icons and update SocialAccountCard component

---
+++ apps/web/src/components/icons/index.ts
@@ -0,0 +1,4 @@
+export { Dailydev } from './dailydev'
+export { Discord } from './discord'
+export { GitHub } from './github'
+export { LinkedIn } from './linkding'
--- apps/web/src/widgets/cards/social-account/social-account.tsx
+++ apps/web/src/widgets/cards/social-account/social-account.tsx
@@ -1,12 +1,46 @@
 'use client'
 
+import { Dailydev, Discord, GitHub, LinkedIn } from '@/components/icons'
 import { DefaultCard } from '../default/default-card'
 import './style.css'
+import Link from 'next/link'
+
+const accounts = [
+	{
+		name: 'LinkedIn',
+		icon: <LinkedIn />,
+		url: 'https://www.linkedin.com/in/your-profile',
+	},
+	{
+		name: 'Discord',
+		icon: <Discord />,
+		url: 'https://discord.com/users/your-profile',
+	},
+	{
+		name: 'daily.dev',
+		icon: <Dailydev />,
+		url: 'https://app.daily.dev/your-profile',
+	},
+	{
+		name: 'GitHub',
+		icon: <GitHub />,
+		url: 'https://github.com/your-profile',
+	},
+]
 
 export const SocialAccountCard = () => {
 	return (
 		<DefaultCard>
 			<DefaultCard.Badge>Social Account</DefaultCard.Badge>
+			<div id='social-account-container'>
+				{accounts.map((account) => {
+					return (
+						<div key={account.name}>
+							<Link href={account.url}>{account.icon}</Link>
+						</div>
+					)
+				})}
+			</div>
 		</DefaultCard>
 	)
 }
