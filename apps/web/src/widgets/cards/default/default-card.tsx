68b36785b0b5744824a36655e0b0d747c695a8e8
Yassir Rouane <yasserro100@gmail.com>
2025-06-22 13:31:23 +0100

    feat: add SocialAccountCard component and update grid layout

--- apps/web/src/widgets/cards/default/default-card.tsx
+++ apps/web/src/widgets/cards/default/default-card.tsx
@@ -1,13 +1,16 @@
 import type { ComponentProps, FC } from 'react'
 import './style.css'
 import { Badge } from '@/components/badge/badge'
+import { cn } from '@sglara/cn'
 
-type DefaultCardProps = ComponentProps<'div'> & {}
+type DefaultCardProps = ComponentProps<'div'> & {
+	withBadge?: boolean
+}
 
 const DefaultCard: FC<DefaultCardProps> & { Badge: FC<ComponentProps<'div'>> } =
-	({ className, children, ...props }) => {
+	({ className, children, withBadge, ...props }) => {
 		return (
-			<div id='default-card' className={`${className} card`} {...props}>
+			<div id='default-card' className={cn(className, 'card')} {...props}>
 				{children}
 			</div>
 		)
---
+++ apps/web/src/widgets/cards/social-account/social-account.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+import { DefaultCard } from '../default/default-card'
+import './style.css'
+
+export const SocialAccountCard = () => {
+	return (
+		<DefaultCard>
+			<DefaultCard.Badge>Social Account</DefaultCard.Badge>
+		</DefaultCard>
+	)
+}
