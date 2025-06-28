7f5a6f9b6ac0fee6a09c5bc51e77327ab177ac26
Yassir Rouane <yasserro100@gmail.com>
2025-06-28 19:23:44 +0100

    feat: add close button to project details page

--- apps/web/src/app/projects/[slug]/page.tsx
+++ apps/web/src/app/projects/[slug]/page.tsx
@@ -1,6 +1,8 @@
 import './style.css'
 import { getAllProjectSlugs, getProjectBySlug } from '@/utils/mdx'
 import { ProjectHeaderPreview } from '@/widgets/projects-page-details'
+import { X } from 'lucide-react'
+import Link from 'next/link'
 import { notFound } from 'next/navigation'
 import { type FC, use } from 'react'
 
@@ -27,6 +29,10 @@ const ProjectPage: FC<ProjectPageProps> = ({ params }) => {
 
 	return (
 		<>
+			<Link href='/' id='project-close-button'>
+				<X />
+			</Link>
+
 			<ProjectHeaderPreview
 				alt={project.meta.headline}
 				src={project.meta.headerPreview}
